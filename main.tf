06f02c59a24faaf66df4735bd0afaea189bbcb35
Mukesh kumar <mukesh.ninja1@gmail.com>
2023-02-16 01:18:05 +0530

    added the Resource Group

---
+++ main.tf
@@ -0,0 +1,14 @@
+
+resource "azurerm_resource_group" "rg" {
+  name                = var.resource_group_name
+  location            = var.resource_group_location
+  tags                = var.tag_map
+}
+
+resource "azurerm_management_lock" "primary_lock" {
+  count      = var.lock_level_value == "" ? 0 : 1
+  name       = "${var.resource_group_name}-level-lock"
+  scope      = azurerm_resource_group.primary_rg.id
+  lock_level = var.lock_level_value
+  notes      = "Resource Group '${var.resource_group_name}' is locked with '${var.lock_level_value}' level"
+}
---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+ type = string
+ description = "Azure resource group name to use"  
+}
+
+variable "location" {
+  type = string
+  description = "Azure region to use"
+}
+
+variable "rg_tags" {
+  type = map(any)
+  description = "value"
+}
